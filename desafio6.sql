dd8e951d2282874cb59402d23214d08bf5b22f7e
isabelanery <isabelanery242@gmail.com>
2022-06-06 19:50:54 -0300

    add query 5

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT 
+  CONVERT(ROUND(MIN(subscription_value), 2), NCHAR) AS faturamento_minimo,
+  CONVERT(MAX(subscription_value), NCHAR) AS faturamento_maximo,
+  CONVERT(ROUND(AVG(subscription_value), 2), NCHAR) AS faturamento_medio,
+  CONVERT(ROUND(SUM(subscription_value), 2), NCHAR) AS faturamento_total
+FROM SpotifyClone.Users AS u
+INNER JOIN SpotifyClone.Subscription_plans AS s
+  ON u.subscription_id = s.subscription_id;
